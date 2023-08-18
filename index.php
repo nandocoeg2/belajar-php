2a17cdbb7191e1b6ca4a75240a52925cb8fb67e1
Fernando Julian <nandokun69@gmail.com>
2023-08-18 16:19:01 +0700

    index

--- index.php
+++ index.php
@@ -1,12 +1,3 @@
 <?php
 
-
-// Looping For
-
-for ($counter = 1; $counter <= 10; $counter++) {
-    echo "Hello World $counter\n";
-}   
-for ($counter = 10; $counter >= 1; $counter--) {
-    echo "Hello World $counter\n";
-}   
-
+echo "Hello World" . PHP_EOL;
