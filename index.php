0011a1b29aaf0944e50becbde6f17d5868da31c2
Fernando Julian <nandokun69@gmail.com>
2023-08-18 15:55:55 +0700

    function

--- index.php
+++ index.php
@@ -9,3 +9,19 @@
 // for ($counter = 10; $counter >= 1; $counter--) {
 //     echo "Hello World $counter\n";
 // }   
+
+
+$testFunction = true;
+
+if ($testFunction) {
+    function test() {
+        echo "Hello World\n";
+    }
+} else {
+    function test() {
+        echo "Hello False\n";
+    }
+}
+
+test();
+
